12c404f8e81bf9448faf26777e187745e23b4e33
Jason <m.jason.liu@outlook.com>
2022-07-12 15:13:39 +0800

    random colors

--- mod.tsx
+++ mod.tsx
@@ -2,6 +2,18 @@
 import { serve } from "https://deno.land/std@0.142.0/http/server.ts";
 import { h, html } from "https://deno.land/x/htm@0.0.2/mod.tsx";
 
+const colors = [
+  "#ff80ed",
+  "#065535",
+  "#ff0000",
+  "#008080",
+  "#ffc0cb",
+  "#b0e0e6",
+  "#ffc3a0",
+  "#66cdaa",
+  "#101010"
+] as readonly string[];
+
 const handler = (req: Request) => html({
   title: "Hello World!",
   body: (
@@ -9,7 +21,7 @@ const handler = (req: Request) => html({
       class="flex flex-col items-center justify-center w-full h-screen"
       style="background-image:url('https://dash.deno.com/assets/background-pattern.svg')"
     >
-      <h1 class="text-4xl font-bold">Hello CGQAQ!</h1>
+      <h1 class="text-4xl font-bold">Hello <span style={{ color: colors[Math.random() * 100_000 % colors.length] }}>CGQAQ</span>!</h1>
       <p class="mt-2 text-lg text-center text-gray-600">Develop Locally, Deploy Globally</p>
       <footer class="fixed bottom-8 w-full h-6 flex items-center justify-center gap-2 text-gray-800">
         Powered by
