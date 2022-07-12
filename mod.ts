4f3fbfdc8fdb2ebc48442b6c8de3ee39be2d717d
Jason <m.jason.liu@outlook.com>
2022-07-12 14:15:04 +0800

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,1 +1,1 @@
-import { serve } from "https://deno.land/std@0.142.0/http/server.ts";  serve((req: Request) => new Response("Hello World"));
+import { serve } from "https://deno.land/std@0.142.0/http/server.ts";  serve((req: Request) => new Response("Hello CGQAQ"));
