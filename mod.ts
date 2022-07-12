a9290a6660e31831b246b29affa9c2e231bddf05
Jason <m.jason.liu@outlook.com>
2022-07-12 14:20:39 +0800

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,1 +1,27 @@
-import { serve } from "https://deno.land/std@0.142.0/http/server.ts";  serve((req: Request) => new Response("Hello CGQAQ"));
+/** @jsx h */
+import { serve } from "https://deno.land/std@0.142.0/http/server.ts";
+import { h, html } from "https://deno.land/x/htm@0.0.2/mod.tsx";
+
+const handler = (req: Request) => html({
+  title: "Hello World!",
+  body: (
+    <div
+      class="flex flex-col items-center justify-center w-full h-screen"
+      style="background-image:url('https://dash.deno.com/assets/background-pattern.svg')"
+    >
+      <h1 class="text-4xl font-bold">Hello CGQAQ!</h1>
+      <p class="mt-2 text-lg text-center text-gray-600">Develop Locally, Deploy Globally</p>
+      <footer class="fixed bottom-8 w-full h-6 flex items-center justify-center gap-2 text-gray-800">
+        Powered by
+        <a
+          class="flex items-center gap-2 text-sm text-black no-underline font-semibold"
+          href="https://deno.com/deploy"
+        >
+          <img alt="Deno" src="https://dash.deno.com/assets/logo.svg" class="w-5" /> Deno Deploy
+        </a>
+      </footer>
+    </div>
+  ),
+});
+
+serve(handler);    
